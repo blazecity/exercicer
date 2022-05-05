f6f950f7768c1a7ec02c85013ab2f6ccde3be801
Jan Baumann <jan.baumann2@outlook.com>
2022-05-05 12:32:43 +0200

    training card

--- app/src/main/java/ch/mobpro/exercicer/data/entity/DistanceUnit.kt
+++ app/src/main/java/ch/mobpro/exercicer/data/entity/DistanceUnit.kt
@@ -1,6 +1,6 @@
 package ch.mobpro.exercicer.data.entity
 
-enum class DistanceUnit(public val multiplicator: Int) {
-    METERS(0),
-    KILOMETERS(1000)
+enum class DistanceUnit(public val multiplicator: Int, public val abbrevation: String) {
+    METERS(1, "m"),
+    KILOMETERS(1000, "km")
 }
--- app/src/main/java/ch/mobpro/exercicer/data/entity/Training.kt
+++ app/src/main/java/ch/mobpro/exercicer/data/entity/Training.kt
@@ -5,6 +5,7 @@ import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import java.time.LocalDate
 import java.util.*
+import kotlin.math.min
 
 @Entity(
     foreignKeys = [
@@ -27,4 +28,25 @@ data class Training(
     var sets: Int? = null,
     var repeats: Int? = null,
     var remarks: String? = null
-)
+) {
+    fun getFormattedTrainingTime(): String? {
+        if (trainingTimeHour == null && trainingTimeMinutes == null && trainingTimeSeconds == null)
+            return null
+
+        val hours = if (trainingTimeHour != null) "${this.trainingTimeHour}:"  else "00:"
+        val minutes = if (trainingTimeMinutes != null) "${this.trainingTimeMinutes}:" else "00:"
+        val seconds = if (trainingTimeSeconds != null) "${this.trainingTimeSeconds}" else "00"
+        return hours + minutes + seconds
+    }
+
+    fun getFormattedTrainingDistance(): String? {
+        if (trainingDistanceInMeters == null) return null
+        val distUnit = if (this.distanceUnit != null) this.distanceUnit else DistanceUnit.METERS
+        return "${this.trainingDistanceInMeters!! / distUnit!!.multiplicator} ${distUnit.abbrevation}"
+    }
+
+    fun getFormattedRepeatsAndSets(): String? {
+        if (sets == null || repeats == null) return null
+        return "${this.sets} x ${this.repeats}"
+    }
+}
